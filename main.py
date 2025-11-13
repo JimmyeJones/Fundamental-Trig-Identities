4819c1b9c305ad3114fb988d60156d2002fa3f89
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-11-13 11:16:11 -0500

    Add Streamlit app for trigonometric calculations
    This is a simple Streamlit app for calculating trigonometric functions based on user input. It allows users to input values for sine, cosine, or tangent and displays the corresponding trigonometric values.

---
+++ main.py
@@ -0,0 +1,86 @@
+#Mattox J
+#Jenkins - AM2
+#A simple Streamlit app to calculate trigonometric functions based on user input.
+#This site is accessible at: https://mattoxj-jenkins-trig-calculator-streamlit-app-4w3s5g.streamlit.app/
+import math
+import streamlit as st
+
+def calc_trig_values(sin_value):
+    try:
+        sin=sin_value
+    except:
+        sin=False
+    try:
+        cos=math.sqrt(1-sin**2)
+    except:
+        cos=False
+    try:
+        tan=sin/cos
+    except:
+        tan=False
+    try:
+        csc=1/sin
+    except:
+        csc=False
+    try:
+        sec=1/cos
+    except:
+        sec=False
+    try:
+        cot=cos/sin
+    except:
+        cot=False
+    values = [["sin(t)", sin], ["cos(t)", cos], ["tan(t)", tan], ["csc(t)", csc], ["sec(t)", sec], ["cot(t)", cot]]
+    return values
+
+st.title("Trigonometric Function Calculator")
+st.text("Made by Mattox J")
+page = st.selectbox("Select your input value", ["sin(t)=?", "cos(t)=?", "tan(t)=?"])
+if page == "sin(t)=?":
+    sin = st.text_input("Enter value")
+    if sin:
+        try:
+            sin = float(sin)
+        except:
+            st.warning("Invalid input")
+        else:
+            if st.button("Calculate"):
+                values = calc_trig_values(sin)
+                for value in values:
+                    if value[1] is not False:
+                        st.info(f"{value[0]} = {round(value[1], 3)}")
+                    else:
+                        st.error(f"{value[0]} is undefined")
+elif page == "cos(t)=?":
+    cos = st.text_input("Enter value")
+    if cos:
+        try:
+            cos = float(cos)
+        except:
+            st.warning("Invalid input")
+        else:
+            sin = math.sqrt(1-cos**2)
+            if st.button("Calculate"):
+                values = calc_trig_values(sin)
+                for value in values:
+                    if value[1] is not False:
+                        st.info(f"{value[0]} = {round(value[1], 3)}")
+                    else:
+                        st.error(f"{value[0]} is undefined")
+elif page == "tan(t)=?":
+    tan = st.text_input("Enter value")
+    if tan:
+        try:
+            tan = float(tan)
+        except:
+            st.warning("Invalid input")
+        else:
+            cos=math.sqrt((tan**2)+1)
+            sin=tan/cos
+            if st.button("Calculate"):
+                values = calc_trig_values(sin)
+                for value in values:
+                    if value[1] is not False:
+                        st.info(f"{value[0]} = {round(value[1], 3)}")
+                    else:
+                        st.error(f"{value[0]} is undefined")
